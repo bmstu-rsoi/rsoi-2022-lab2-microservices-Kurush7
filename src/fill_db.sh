64d59bdc70bf3af9f062c541807f0b494ff12ebb
Kurush7 <ze17@yandex.ru>
2022-10-28 22:16:46 +0300

    deploy update

--- src/fill_db.sh
+++ src/fill_db.sh
@@ -1,4 +1,5 @@
-docker ps
+sleep 5
+docker ps -a
 docker exec -i postgres psql -U program libraries < test/libraries.dump
 docker exec -i postgres psql -U program ratings < test/ratings.dump
 docker exec -i postgres psql -U program reservations < test/reservations.dump
